e2ea9d52a9578c3c2ca17f02f179062ad27340ef
Misaka-L <lipww1234@foxmail.com>
2023-01-27 02:00:52 +0800

    移动仪表源代码位置，添加 RMP，修复无法显示无线电频率的问题

---
+++ Avionics/Instruments/RMP/Script/RMP.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine.UI;
+using URC;
+
+namespace A320VAU
+{
+    public class RMP : UdonSharpBehaviour
+    {
+        public Transceiver VHF1;
+
+        public Text ActiveFrequencyText;
+
+        private void LateUpdate() {
+            ActiveFrequencyText.text = VHF1.Frequency.ToString("000.000");
+        }
+    }
+}
