1e3ccf77d89867b3c1aa8374d6264a48878b05bf
Misaka-L <lipww1234@foxmail.com>
2023-04-16 02:21:01 +0800

    feature: navigation display

---
+++ Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/EFIS/ND/Script/Pages/ARCPage.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace A320VAU.ND.Pages
+{
+    public class ARCPage : UdonSharpBehaviour
+    {
+        void Start()
+        {
+        
+        }
+    }
+}
