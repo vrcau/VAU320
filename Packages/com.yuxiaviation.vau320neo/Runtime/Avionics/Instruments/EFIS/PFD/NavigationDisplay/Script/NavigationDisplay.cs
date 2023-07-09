77cb7dbb9befc432d7790baca2351eca87385128
Misaka-L <lipww1234@foxmail.com>
2023-07-09 18:53:47 +0800

    refactor(pfd): cleanup NavigationDisplay file, use injector

--- Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/EFIS/PFD/NavigationDisplay/Script/NavigationDisplay.cs
+++ Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/EFIS/PFD/NavigationDisplay/Script/NavigationDisplay.cs
@@ -1,4 +1,5 @@
-﻿using UdonSharp;
+﻿using A320VAU.Common;
+using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 using VirtualAviationJapan;
@@ -7,21 +8,23 @@ namespace A320VAU.PFD {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     [DefaultExecutionOrder(90)] // After Nav Selector, Before Instruments
     public class NavigationDisplay : UdonSharpBehaviour {
-        private const int UPDATE_STEP_COUNT = 4;
-        public NavSelector navSelector;
+        private DependenciesInjector _injector;
+        private FMGC.FMGC _fmgc;
+        private NavSelector _ils;
 
         [Tooltip("信息显示")]
         public Text NaviInfo;
 
         public int updateIntervalFrames = 10;
-        private float magneticDeclination;
         private int updateIntervalOffsetFrames;
+        
+        private const int UPDATE_STEP_COUNT = 4;
 
         private void Start() {
-            if (!navSelector) navSelector = GetComponentInParent<NavSelector>();
-            var navaidDatabaseObj = GameObject.Find(nameof(NavaidDatabase));
-            if (navaidDatabaseObj)
-                magneticDeclination = navaidDatabaseObj.GetComponent<NavaidDatabase>().magneticDeclination;
+            _injector = DependenciesInjector.GetInstance(this);
+            _fmgc = _injector.fmgc;
+            _ils = _fmgc.radNav.ILS;
+            
             NaviInfo.text = "---\n--- < size = 14 > .-- </ size >\n- < size = 14 > .- </ size >;";
         }
 
@@ -36,22 +39,16 @@ namespace A320VAU.PFD {
         }
 
         private Vector3 GetNavaidPosition() {
-            var t = navSelector.NavaidTransform;
+            var t = _ils.NavaidTransform;
             return (t ? t : transform).position;
         }
 
-        private int GetBearing() {
-            var direction = Vector3.ProjectOnPlane(GetNavaidPosition() - transform.position, Vector3.up).normalized;
-            var bearing = (Mathf.RoundToInt(Vector3.SignedAngle(Vector3.forward, direction, Vector3.up)) + 360) % 360;
-            return bearing == 0 ? 360 : bearing;
-        }
-
         private void UpdateText() {
-            var distance = navSelector.HasDME ? Vector3.Distance(transform.position, GetNavaidPosition()) / 1852.0f : 0;
+            var distance = _ils.HasDME ? Vector3.Distance(transform.position, GetNavaidPosition()) / 1852.0f : 0;
             var frequency =
-                navSelector.Index >= 0 ? navSelector.database.frequencies[navSelector.Index] : 999.9; //TODO 错误频率怎么显示
+                _ils.Index >= 0 ? _ils.database.frequencies[_ils.Index] : 999.9; //TODO 错误频率怎么显示
             NaviInfo.text = string.Format("{0}\n{1:f0}<size=14>.{2:f0}</size>\n{3:f1}",
-                navSelector.Identity ?? "---",
+                _ils.Identity ?? "---",
                 (int)frequency,
                 (frequency - (int)frequency) * 10,
                 distance
