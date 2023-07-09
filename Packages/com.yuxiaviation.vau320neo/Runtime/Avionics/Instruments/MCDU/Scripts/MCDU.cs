96ebd57928984a8972760c5b78ea2649567a639a
Misaka-L <lipww1234@foxmail.com>
2023-07-09 19:21:21 +0800

    refactor(mcdu): add [PublicAPI] attribute

--- Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/MCDU/Scripts/MCDU.cs
+++ Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/MCDU/Scripts/MCDU.cs
@@ -1,4 +1,5 @@
-﻿using UdonSharp;
+﻿using JetBrains.Annotations;
+using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,7 +27,8 @@ namespace A320VAU.MCDU {
         private bool _hasMessage;
         private float _lastUpdate;
         private string _mcduMessage = "";
-        public MCDUPage CurrentPage { get; private set; }
+
+        [PublicAPI] public MCDUPage CurrentPage { get; private set; }
 
         private void Start() {
             ToPage(McduMenuPage);
@@ -40,6 +42,7 @@ namespace A320VAU.MCDU {
                 CurrentPage.OnPageUpdate();
         }
 
+        [PublicAPI]
         public void ToPage(UdonSharpBehaviour page) {
             if (page == null) return;
 
@@ -48,6 +51,7 @@ namespace A320VAU.MCDU {
             CurrentPage.OnPageInit(this);
         }
 
+        [PublicAPI]
         public void ClearDisplay() {
             titleLineText.text = "";
             l1Text.text = "";
@@ -79,6 +83,7 @@ namespace A320VAU.MCDU {
             r6Label.text = "";
         }
 
+        [PublicAPI]
         public void SendMCDUMessage(string content) {
             _hasMessage = true;
             _mcduMessage = content;
@@ -86,11 +91,13 @@ namespace A320VAU.MCDU {
             scratchpadText.text = content;
         }
 
+        [PublicAPI]
         public void ClearInput() {
             scratchpad = "";
             scratchpadText.text = scratchpad;
         }
 
+        [PublicAPI]
         public void Input(string content) {
             scratchpad += content;
             scratchpadText.text = scratchpad;
@@ -195,50 +202,62 @@ namespace A320VAU.MCDU {
 
     #region PageKeys
 
+        [PublicAPI]
         public void DIR() {
             ToPage(DirPage);
         }
 
+        [PublicAPI]
         public void Prog() {
             ToPage(ProgPage);
         }
 
+        [PublicAPI]
         public void Perf() {
             ToPage(PerfPage);
         }
 
+        [PublicAPI]
         public void Init() {
             ToPage(InitPage);
         }
 
+        [PublicAPI]
         public void Data() {
             ToPage(DataPage);
         }
 
+        [PublicAPI]
         public void FPLN() {
             ToPage(FPlnPage);
         }
 
+        [PublicAPI]
         public void SecFPLN() {
             ToPage(SecFPlnPage);
         }
 
+        [PublicAPI]
         public void RadNav() {
             ToPage(RadNavPage);
         }
 
+        [PublicAPI]
         public void FuelPred() {
             ToPage(FuelPredPage);
         }
 
+        [PublicAPI]
         public void AtcComm() {
             ToPage(AtcCommPage);
         }
 
+        [PublicAPI]
         public void McduMenu() {
             ToPage(McduMenuPage);
         }
 
+        [PublicAPI]
         public void Airport() {
             ToPage(FPlnPage);
         }
@@ -247,18 +266,22 @@ namespace A320VAU.MCDU {
 
     #region SlewKeys
 
+        [PublicAPI]
         public void Up() {
             CurrentPage.Up();
         }
 
+        [PublicAPI]
         public void Down() {
             CurrentPage.Down();
         }
 
+        [PublicAPI]
         public void PrevPage() {
             CurrentPage.PrevPage();
         }
 
+        [PublicAPI]
         public void NextPage() {
             CurrentPage.NextPage();
         }
@@ -267,50 +290,62 @@ namespace A320VAU.MCDU {
 
     #region AlphaNumberKeys
 
+        [PublicAPI]
         public void Number0() {
             Input("0");
         }
 
+        [PublicAPI]
         public void Number1() {
             Input("1");
         }
 
+        [PublicAPI]
         public void Number2() {
             Input("2");
         }
 
+        [PublicAPI]
         public void Number3() {
             Input("3");
         }
 
+        [PublicAPI]
         public void Number4() {
             Input("4");
         }
 
+        [PublicAPI]
         public void Number5() {
             Input("5");
         }
 
+        [PublicAPI]
         public void Number6() {
             Input("6");
         }
 
+        [PublicAPI]
         public void Number7() {
             Input("7");
         }
 
+        [PublicAPI]
         public void Number8() {
             Input("8");
         }
 
+        [PublicAPI]
         public void Number9() {
             Input("9");
         }
 
+        [PublicAPI]
         public void Point() {
             Input(".");
         }
 
+        [PublicAPI]
         public void PlusOrNeg() {
             if (scratchpad.Length != 0)
                 switch (scratchpad[scratchpad.Length - 1]) {
@@ -334,125 +369,155 @@ namespace A320VAU.MCDU {
 
     #region LetterKeys
 
+        [PublicAPI]
         public void InputA() {
             Input("A");
         }
 
+        [PublicAPI]
         public void InputB() {
             Input("B");
         }
 
+        [PublicAPI]
         public void InputC() {
             Input("C");
         }
 
+        [PublicAPI]
         public void InputD() {
             Input("D");
         }
 
+        [PublicAPI]
         public void InputE() {
             Input("E");
         }
 
+        [PublicAPI]
         public void InputF() {
             Input("F");
         }
 
+        [PublicAPI]
         public void InputG() {
             Input("G");
         }
 
+        [PublicAPI]
         public void InputH() {
             Input("H");
         }
 
+        [PublicAPI]
         public void InputI() {
             Input("I");
         }
 
+        [PublicAPI]
         public void InputJ() {
             Input("J");
         }
 
+        [PublicAPI]
         public void InputK() {
             Input("K");
         }
 
+        [PublicAPI]
         public void InputL() {
             Input("L");
         }
 
+        [PublicAPI]
         public void InputM() {
             Input("M");
         }
 
+        [PublicAPI]
         public void InputN() {
             Input("N");
         }
 
+        [PublicAPI]
         public void InputO() {
             Input("O");
         }
 
+        [PublicAPI]
         public void InputP() {
             Input("P");
         }
 
+        [PublicAPI]
         public void InputQ() {
             Input("Q");
         }
 
+        [PublicAPI]
         public void InputR() {
             Input("R");
         }
 
+        [PublicAPI]
         public void InputS() {
             Input("S");
         }
 
+        [PublicAPI]
         public void InputT() {
             Input("T");
         }
 
+        [PublicAPI]
         public void InputU() {
             Input("U");
         }
 
+        [PublicAPI]
         public void InputV() {
             Input("V");
         }
 
+        [PublicAPI]
         public void InputW() {
             Input("W");
         }
 
+        [PublicAPI]
         public void InputX() {
             Input("X");
         }
 
+        [PublicAPI]
         public void InputY() {
             Input("Y");
         }
 
+        [PublicAPI]
         public void InputZ() {
             Input("Z");
         }
 
     #endregion
 
+        [PublicAPI]
         public void ForwardSlash() {
             Input("/");
         }
 
+        [PublicAPI]
         public void Space() {
             Input(" ");
             // It's a space
         }
 
+        [PublicAPI]
         public void Overfly() {
             // TODO
         }
 
+        [PublicAPI]
         public void CLR() {
             if (_hasMessage) {
                 scratchpadText.text = scratchpad;
