7f92a2e4b7abcd193890be9136fe4a4e39789464
Misaka-L <lipww1234@foxmail.com>
2023-09-16 04:25:08 +0800

    feature: efb

---
+++ Packages/com.yuxiaviation.vau320neo/Runtime/EFB/Scripts/SimplePageNavigator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VirtualFlightBag {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SimplePageNavigator : UdonSharpBehaviour {
+        public bool isNetworked;
+
+        public Toggle[] navItems = { };
+        public GameObject[] pages = { };
+
+        private void Start() {
+            _InvokeNavigatorUpdate();
+        }
+
+        public void _InvokeNavigatorUpdate() {
+            for (var index = 0; index < navItems.Length; index++) {
+                var navItem = navItems[index];
+                var page = pages[index];
+
+                if (!navItem.isOn) continue;
+
+                ResetAllPages();
+                page.SetActive(true);
+
+                return;
+            }
+        }
+
+        private void ResetAllPages() {
+            foreach (var page in pages)
+                page.SetActive(false);
+        }
+    }
+}
