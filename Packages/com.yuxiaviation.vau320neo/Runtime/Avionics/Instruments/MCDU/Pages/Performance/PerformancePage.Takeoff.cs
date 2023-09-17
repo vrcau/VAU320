0cbe627d360034d9eacf1ab90b8d1fc9bc038dd7
Misaka-L <lipww1234@foxmail.com>
2023-09-17 17:53:46 +0800

    feature(fmgc): performance ui (w.i.p)

---
+++ Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Instruments/MCDU/Pages/Performance/PerformancePage.Takeoff.cs
@@ -0,0 +1,34 @@
+﻿using A320VAU.Common;
+
+namespace A320VAU.MCDU {
+    public partial class PerformancePage {
+        private void TakeoffUI() {
+            _mcdu.titleLineText.text = "TAKEOFF";
+
+            _mcdu.l1Label.text = "V1   FLP PETR";
+            _mcdu.l1Text.text = $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.v1.ToString()}</color>     F=<color={AirbusAvionicsTheme.Green}>147</color>";
+            _mcdu.l2Label.text = "VR   SLT PETR";
+            _mcdu.l2Text.text = $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.vr.ToString()}</color>     S=<color={AirbusAvionicsTheme.Green}>191</color>";
+            _mcdu.l3Label.text = "V2      CLEAN";
+            _mcdu.l3Text.text = $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.v2.ToString()}</color>     O=<color={AirbusAvionicsTheme.Green}>212</color>";
+            _mcdu.l4Label.text = "TRANS ALT";
+            _mcdu.l4Text.text = $"<color={AirbusAvionicsTheme.Blue}>6000</color>";
+            _mcdu.l5Label.text = "THR RED/ACC";
+            _mcdu.l5Text.text = $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.reduceThrustAltitude.ToString()}/{_fmgc.performance.accelerateAltitude.ToString()}</color>";
+
+            // _mcdu.r2Label.text = "TO SHIFT";
+            // _mcdu.r2Text.text = $"<color={AirbusAvionicsTheme.Blue}>[M][  ]*</color>";
+            _mcdu.r3Label.text = "FLAPS/THS";
+            _mcdu.r3Text.text =
+                $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.takeoffFlapSetting}/[   ]</color>";
+            _mcdu.r4Label.text = "FLEX TO TEMP";
+            _mcdu.r4Text.text = $"<color={AirbusAvionicsTheme.Blue}>[   ]</color>";
+            _mcdu.r5Label.text = "ENG OUT ACC";
+            _mcdu.r5Text.text =
+                $"<color={AirbusAvionicsTheme.Blue}>{_fmgc.performance.engineOutAccelerateAltitude}</color>";
+
+            _mcdu.l6Label.text = "NEXT";
+            _mcdu.l6Text.text = "PHASE>";
+        }
+    }
+}
---
+++ Packages/com.yuxiaviation.vau320neo/Runtime/Avionics/Systems/FMGC/FMGCPerformance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using A320VAU.Common;
+using UdonSharp;
+using UnityEngine.Serialization;
+
+namespace A320VAU.FMGC {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class FMGCPerformance : UdonSharpBehaviour {
+        public FMGC fmgc;
+
+        private DependenciesInjector _injector;
+
+        public int v1 = 143;
+        public int vr = 152;
+        public int v2 = 162;
+
+        public int reduceThrustAltitude = 1500;
+        public int accelerateAltitude = 1500;
+        public int engineOutAccelerateAltitude = 1500;
+
+        public int flexTemperature;
+        public int takeoffFlapSetting = 1;
+
+        public int vapp = 132;
+
+        public int minimumDescentDecisionAltitude;
+        public int decisionHeight = 200;
+
+        public FMGCPerformanceLandingConfig landingConfig = FMGCPerformanceLandingConfig.Full;
+
+        private void Start() {
+            _injector = DependenciesInjector.GetInstance(this);
+
+            _injector.systemEventBus.RegisterSaccEvent(this);
+            _injector.systemEventBus.Register(this);
+        }
+
+        public void EventBus_FlightPhaseChanged() {
+            if (fmgc.flightPhase.CurrentFlightPhase == FlightPhase.Done)
+                ReInit();
+        }
+
+        private void ReInit() {
+
+        }
+    }
+
+    public enum FMGCPerformanceLandingConfig {
+        Full,
+        Conf3
+    }
+}
